32fd993535e1151e35535813bff95f3a41719015
unknown <Blackjacknick@gmail.com>
2019-03-16 11:30:35 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,15 @@
-console.log("Up and running!");
-var cardOne = "queen"
-var cardTwo = "king"
-var cardThree = "queen"
-var cardFour = "king"
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0]; 
+cardsInPlay.push(cardOne);
+console.log(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log(cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+ alert("You Found a Match!")
+}
+ else if (cardsInPlay[0] !== cardsInPlay[1]) {
+  alert("Oops! Guess Again!")}
+}
