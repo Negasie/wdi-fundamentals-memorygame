d5323d5b44fde8197917d87353e4ea2e8bab989f
unknown <Blackjacknick@gmail.com>
2019-03-16 13:52:52 -0600

    created game logic Extra Error message

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0]; 
-cardsInPlay.push(cardOne);
-console.log(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log(cardTwo);
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
- alert("You Found a Match!")
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
- else if (cardsInPlay[0] !== cardsInPlay[1]) {
-  alert("Oops! Guess Again!")}
+}
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+	flipCard(0);
+	flipCard(2);
+    if (cardsInPlay.length === 2) {
+  	if (cardsInPlay[0] === cardsInPlay[1]) {
+   alert("You Found a Match!")
+}
+   else if (cardsInPlay[0] !== cardsInPlay[1]) {
+   alert("Oops! Guess Again!")}
 }
